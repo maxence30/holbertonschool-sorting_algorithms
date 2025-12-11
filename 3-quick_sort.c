ac64688a327591d7de1acb9d7708f49cad56de0e
maxence30 <henous.maxence@outlook.fr>
2025-12-11 13:34:51 +0000

    CBONLAJECROISJAIENFINFINIMGL

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -11,65 +11,64 @@
  */
 int lomuto_partition(int *array, int low, int high, size_t size)
 {
-    int pivot = array[high];
-    int i = low;
-    int j, temp;
+	int pivot = array[high];
+	int i = low;
+	int j, temp;
 
-    for (j = low; j < high; j++)
-    {
-        if (array[j] < pivot)
-        {
-            if (i != j)
-            {
-                temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-                print_array(array, size);
-            }
-            i++;
-        }
-    }
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			if (i != j)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+			i++;
+		}
+	}
 
-    if (array[i] != array[high])
-    {
-        temp = array[i];
-        array[i] = array[high];
-        array[high] = temp;
-        print_array(array, size);
-    }
+	if (array[i] != array[high])
+	{
+		temp = array[i];
+		array[i] = array[high];
+		array[high] = temp;
+		print_array(array, size);
+	}
 
-    return (i);
+	return (i);
 }
 
 /**
- * quicksort_rec - recursively sorts an array using quick sort
+ * quicksort_rec - recursively sorts using quick sort
  * @array: array to sort
  * @low: starting index
  * @high: ending index
- * @size: size of the full array
+ * @size: size of array
  */
 void quicksort_rec(int *array, int low, int high, size_t size)
 {
-    int pivot;
+	int pivot;
 
-    if (low < high)
-    {
-        pivot = lomuto_partition(array, low, high, size);
-        quicksort_rec(array, low, pivot - 1, size);
-        quicksort_rec(array, pivot + 1, high, size);
-    }
+	if (low < high)
+	{
+		pivot = lomuto_partition(array, low, high, size);
+		quicksort_rec(array, low, pivot - 1, size);
+		quicksort_rec(array, pivot + 1, high, size);
+	}
 }
 
 /**
- * quick_sort - sorts an array of integers in ascending order
- *              using the Quick sort algorithm
+ * quick_sort - sorts an array of integers using quicksort
  * @array: array to sort
  * @size: size of array
  */
 void quick_sort(int *array, size_t size)
 {
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    quicksort_rec(array, 0, size - 1, size);
+	quicksort_rec(array, 0, size - 1, size);
 }
